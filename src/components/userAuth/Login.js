6a4268cb5834e0363c003f8c90d9730274dddbe0
nalanagendra <nalanagendrakamath@gmail.com>
2021-11-12 19:25:26 +0530

    login form created and client side validation done

--- src/components/userAuth/Login.js
+++ src/components/userAuth/Login.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string().required("Required!").email("Enter valid email"),
+  password: Yup.string().required("Required!!"),
+});
+
+const onSubmit = (values) => {
+  console.log(values);
+};
 
 const Login = (props) => {
   return (
     <div>
       <h2>Login component</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form>
+          <Field name="email" placeholder="Enter email" />
+          <br />
+          <ErrorMessage name="email" />
+          <br />
+          <Field name="password" type="password" placeholder="Enter password" />
+          <br />
+          <ErrorMessage name="password" />
+          <br />
+          <button type="submit">Login</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
