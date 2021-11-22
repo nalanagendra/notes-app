ab417feba832d28287bd64c7ccd0c6ffaf8d1601
nalanagendra <nalanagendrakamath@gmail.com>
2021-11-22 15:14:01 +0530

    Account component styled

--- src/components/userAuth/Account.js
+++ src/components/userAuth/Account.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { startFetchAccountInfo } from "../../actions/userActions";
+import MessageBox from "../resusableComponents/MessageBox";
 
 const Account = (props) => {
   const dispatch = useDispatch();
@@ -17,19 +18,34 @@ const Account = (props) => {
   }, []);
 
   return (
-    <div>
-      <h2>Account Information</h2>
-      {message && <div>{message}</div>}
-      {isLoading && <div>Loading..</div>}
-      {!isLoading && !errorMessage ? (
-        <div>
-          <p>Id: {accountData._id}</p>
-          <h3>User Name: {accountData.username}</h3>
-          <p>Email: {accountData.email}</p>
+    <div className="container mt-5">
+      <div className="row align-items-center justify-content-center">
+        <div className="col-md-6 col-lg-4">
+          {isLoading && (
+            <div className="text-center"> 
+              <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+              <div><strong>Loading...</strong></div>
+            </div>
+          )} 
+
+          {message && <div className="text-center alert alert-success" role="alert">{message}</div> }
+
+          {errorMessage &&<div className="text-center alert alert-danger" role="alert" role="alert">{errorMessage}</div> }
+
+          {(!isLoading && !errorMessage) && (
+            <div className="card text-center">
+              <h2 className="card-header">Account Information</h2>
+              <div className="card-body p-sm-5">
+                <h4 className="card-title">User Name: {accountData.username}</h4>
+                <p className="card-text">Id: {accountData._id}</p>
+                <p className="card-text">Email: {accountData.email}</p>
+              </div>
+            </div>
+          )}
         </div>
-      ) : (
-        <div>{errorMessage}</div>
-      )}
+      </div>
     </div>
   );
 };
